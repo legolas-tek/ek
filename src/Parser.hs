1faba02af40c5e99c8fe2bb5e117b2b2c9775bd6
Emil Pedersen <emil.pedersen@epitech.eu>
2023-12-06 19:04:22 +0100

    feat: refactor all parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -15,72 +15,62 @@ type ParserError = String
 
 type Parser' a = String -> Either ParserError (a, String)
 
-parseChar :: Char -> Parser' Char
-parseChar expected [] = Left $ "Expected '" ++ [expected] ++ "' but found EOF"
-parseChar expected (x:xs)
-    | x == expected = Right (x, xs)
-    | otherwise     = Left $ "Expected '" ++ [expected] ++ "' but found '" ++ [x] ++ "'"
+parseOneIf :: (Char -> Bool) -> Parser' Char
+parseOneIf _ [] = Left "Unexpected EOF"
+parseOneIf predicate (x:xs)
+  | predicate x = Right (x, xs)
+  | otherwise   = Left $ "Unexpected '" ++ [x] ++ "'"
 
-parseAnyChar :: String -> Parser' Char
-parseAnyChar allowed []
-  = Left $ "Expected one of '" ++ allowed ++ "' but found EOF"
-parseAnyChar allowed (x:xs)
-  | x `elem` allowed = Right (x, xs)
-  | otherwise        = Left $ "Expected one of '" ++ allowed ++ "' but found '" ++ [x] ++ "'"
+parseChar :: Char -> Parser Char
+parseChar expected = Parser $ parseOneIf (== expected)
 
-parseOr :: Parser' a -> Parser' a -> Parser' a
-parseOr p1 p2 input = p1 input `combine` p2 input
+parseAnyChar :: String -> Parser Char
+parseAnyChar allowed = Parser $ parseOneIf (`elem` allowed)
+
+parseOr' :: Parser' a -> Parser' a -> Parser' a
+parseOr' p1 p2 input = p1 input `combine` p2 input
   where
     combine (Left _) (Right ok) = Right ok
     combine (Right ok) _        = Right ok
     combine (Left err1) (Left err2) = Left $ err1 ++ " or " ++ err2
 
-parseAnd :: Parser' a -> Parser' b -> Parser' (a, b)
-parseAnd p1 p2 input = p1 input >>= applyNext
+parseAnd' :: Parser' a -> Parser' b -> Parser' (a, b)
+parseAnd' p1 p2 input = p1 input >>= applyNext
   where applyNext (ok1, rest) = p2 rest >>= finish ok1
         finish ok1 (ok2, rest) = Right ((ok1, ok2), rest)
 
-parseAndWith :: (a -> b -> c) -> Parser' a -> Parser' b -> Parser' c
-parseAndWith f p1 p2 input = parseAnd p1 p2 input >>= apply
+parseAndWith' :: (a -> b -> c) -> Parser' a -> Parser' b -> Parser' c
+parseAndWith' f p1 p2 input = parseAnd' p1 p2 input >>= apply
   where apply ((ok1, ok2), rest) = Right (f ok1 ok2, rest)
 
-parseMany :: Parser' a -> Parser' [a]
-parseMany p = parseOr (parseAndWith (:) p (parseMany p)) parseZero
-  where parseZero input = Right ([], input)
+parseMany :: Parser a -> Parser [a]
+parseMany p = parseSome p <|> parseZero
+  where parseZero = pure []
 
-parseSome :: Parser' a -> Parser' [a]
-parseSome p = parseAndWith (:) p (parseMany p)
+parseSome :: Parser a -> Parser [a]
+parseSome p = p >>= \n -> (n:) <$> parseMany p
 
-parseUInt :: Parser' Integer
-parseUInt input = parseSome (parseAnyChar ['0'..'9']) input >>= convert
-  where convert (digits, rest) = Right (read digits, rest)
+parseUInt :: Parser Integer
+parseUInt = read <$> parseSome (parseAnyChar ['0'..'9'])
 
-parseInt :: Parser' Integer
-parseInt = parseOr parseNegative parseUInt
-  where parseNegative = parseAndWith negate (parseChar '-') parseUInt
-        negate _ i = -i
+parseInt :: Parser Integer
+parseInt = (parseChar '-' >> negate <$> parseUInt) <|> parseUInt
 
-parsePair :: Parser' a -> Parser' (a, a)
-parsePair p input = do
-  (_, next) <- parseChar '(' input
-  (x, next) <- p next
-  (_, next) <- parseMany (parseChar ' ') next
-  (y, next) <- p next
-  (_, next) <- parseChar ')' next
-  return ((x, y), next)
+parsePair :: Parser a -> Parser (a, a)
+parsePair p = do
+  _ <- parseChar '('
+  x <- p
+  _ <- parseMany (parseChar ' ')
+  y <- p
+  _ <- parseChar ')'
+  return (x, y)
 
-parseList :: Parser' a -> Parser' [a]
-parseList p input = do
-  (_, next) <- parseChar '(' input
-  (x, next) <- parseSeparatedValues (parseMany (parseChar ' ')) p next
-  (_, next) <- parseChar ')' next
-  return (x, next)
-
--- Parse a list of values separated by a separator, without the parenthesis
-parseSeparatedValues :: Parser' b -> Parser' a -> Parser' [a]
-parseSeparatedValues sep p = parseAndWith sec sep (parseOr parseNonEmpty parseEmpty)
-  where parseEmpty next = Right ([], next)
-        parseNonEmpty = parseAndWith (:) p (parseSeparatedValues sep p)
+parseList :: Parser a -> Parser [a]
+parseList p = do
+  _ <- parseChar '('
+  x <- parseMany (parseMany (parseChar ' ') >> p)
+  _ <- parseChar ')'
+  return x
 
 sec :: a -> b -> b
 sec _ b = b
@@ -93,12 +83,12 @@ instance Functor Parser where
 
 instance Applicative Parser where
   pure x = Parser $ \input -> Right (x, input)
-  liftA2 fct p1 p2 = Parser $ parseAndWith fct (runParser p1) (runParser p2)
+  liftA2 fct p1 p2 = Parser $ parseAndWith' fct (runParser p1) (runParser p2)
 
 instance Alternative Parser where
   -- empty should be an empty production, but we can't generalize it
   empty = Parser $ \_ -> Left "Empty parser"
-  p1 <|> p2 = Parser $ runParser p1 `parseOr` runParser p2
+  p1 <|> p2 = Parser $ runParser p1 `parseOr'` runParser p2
 
 instance Monad Parser where
   p >>= fct = Parser $ runParser p >=> \(a, rest) -> runParser (fct a) rest
