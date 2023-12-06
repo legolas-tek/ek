5bfa34566f7b7b9bc2bc7a43046cca4a0bd57e28
Emil Pedersen <emil.pedersen@epitech.eu>
2023-12-06 11:43:09 +0100

    feat: parser fmap

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,57 +6,58 @@
 -}
 
 module Parser where
+import Control.Monad ( (>=>) )
 
 type ParserError = String
 
-type Parser a = String -> Either ParserError (a, String)
+type Parser' a = String -> Either ParserError (a, String)
 
-parseChar :: Char -> Parser Char
+parseChar :: Char -> Parser' Char
 parseChar expected [] = Left $ "Expected '" ++ [expected] ++ "' but found EOF"
 parseChar expected (x:xs)
     | x == expected = Right (x, xs)
     | otherwise     = Left $ "Expected '" ++ [expected] ++ "' but found '" ++ [x] ++ "'"
 
-parseAnyChar :: String -> Parser Char
+parseAnyChar :: String -> Parser' Char
 parseAnyChar allowed []
   = Left $ "Expected one of '" ++ allowed ++ "' but found EOF"
 parseAnyChar allowed (x:xs)
   | x `elem` allowed = Right (x, xs)
   | otherwise        = Left $ "Expected one of '" ++ allowed ++ "' but found '" ++ [x] ++ "'"
 
-parseOr :: Parser a -> Parser a -> Parser a
+parseOr :: Parser' a -> Parser' a -> Parser' a
 parseOr p1 p2 input = p1 input `combine` p2 input
   where
     combine (Left _) (Right ok) = Right ok
     combine (Right ok) _        = Right ok
     combine (Left err1) (Left err2) = Left $ err1 ++ " or " ++ err2
 
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
+parseAnd :: Parser' a -> Parser' b -> Parser' (a, b)
 parseAnd p1 p2 input = p1 input >>= applyNext
   where applyNext (ok1, rest) = p2 rest >>= finish ok1
         finish ok1 (ok2, rest) = Right ((ok1, ok2), rest)
 
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+parseAndWith :: (a -> b -> c) -> Parser' a -> Parser' b -> Parser' c
 parseAndWith f p1 p2 input = parseAnd p1 p2 input >>= apply
   where apply ((ok1, ok2), rest) = Right (f ok1 ok2, rest)
 
-parseMany :: Parser a -> Parser [a]
+parseMany :: Parser' a -> Parser' [a]
 parseMany p = parseOr (parseAndWith (:) p (parseMany p)) parseZero
   where parseZero input = Right ([], input)
 
-parseSome :: Parser a -> Parser [a]
+parseSome :: Parser' a -> Parser' [a]
 parseSome p = parseAndWith (:) p (parseMany p)
 
-parseUInt :: Parser Integer
+parseUInt :: Parser' Integer
 parseUInt input = parseSome (parseAnyChar ['0'..'9']) input >>= convert
   where convert (digits, rest) = Right (read digits, rest)
 
-parseInt :: Parser Integer
+parseInt :: Parser' Integer
 parseInt = parseOr parseNegative parseUInt
   where parseNegative = parseAndWith negate (parseChar '-') parseUInt
         negate _ i = -i
 
-parsePair :: Parser a -> Parser (a, a)
+parsePair :: Parser' a -> Parser' (a, a)
 parsePair p input = do
   (_, next) <- parseChar '(' input
   (x, next) <- p next
@@ -65,7 +66,7 @@ parsePair p input = do
   (_, next) <- parseChar ')' next
   return ((x, y), next)
 
-parseList :: Parser a -> Parser [a]
+parseList :: Parser' a -> Parser' [a]
 parseList p input = do
   (_, next) <- parseChar '(' input
   (x, next) <- parseSeparatedValues (parseMany (parseChar ' ')) p next
@@ -73,10 +74,17 @@ parseList p input = do
   return (x, next)
 
 -- Parse a list of values separated by a separator, without the parenthesis
-parseSeparatedValues :: Parser b -> Parser a -> Parser [a]
+parseSeparatedValues :: Parser' b -> Parser' a -> Parser' [a]
 parseSeparatedValues sep p = parseAndWith sec sep (parseOr parseNonEmpty parseEmpty)
   where parseEmpty next = Right ([], next)
         parseNonEmpty = parseAndWith (:) p (parseSeparatedValues sep p)
 
 sec :: a -> b -> b
 sec _ b = b
+
+newtype Parser a = Parser { runParser :: Parser' a }
+
+instance Functor Parser where
+  fmap fct p = Parser $ runParser p >=> Right . mapFst fct
+    where mapFst f (x, y) = (f x, y)
+
