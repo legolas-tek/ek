d66f9e289fdc33299f67cf06e2597b9f54491eae
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2023-12-08 15:01:33 +0100

    chore: AstError changed to SyntaxError

--- src/Ast.hs
+++ src/Ast.hs
@@ -9,11 +9,10 @@ data Ast = Define String Ast
           | Symbol String
           | Lambda [String] [Ast]
           | Call Ast [Ast]
-          deriving Show
 
-type AstError = String
+type SyntaxError = String
 
-sexprToAST :: SExpr.SExpr -> Either AstError Ast
+sexprToAST :: SExpr.SExpr -> Either SyntaxError Ast
 sexprToAST (SExpr.Symbol s) = Right (Ast.Symbol s)
 sexprToAST (SExpr.IntegerLit i) = Right (Ast.IntegerLit i)
 sexprToAST (SExpr.List [SExpr.Symbol "define", SExpr.Symbol s, v]) = sexprToAST v >>= \e -> Right (Ast.Define s e)
@@ -22,4 +21,4 @@ sexprToAST (SExpr.List (SExpr.Symbol "lambda":(SExpr.List args):xs)) = do
   body <- mapM sexprToAST xs
   return (Ast.Lambda argsList body)
 sexprToAST (SExpr.List (SExpr.Symbol x:xs)) = mapM sexprToAST xs >>= \e -> Right (Call (Ast.Symbol x) e)
-sexprToAST _ = Left "Not a valid AST"
+sexprToAST _ = Left "Invalid syntax"
