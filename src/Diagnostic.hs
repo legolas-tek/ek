b67eea81530c3290a0410e2b631ac113707253f4
Yunse LEE <yunselee@gmail.com>
2023-12-23 14:27:23 +0100

    feat: add diagnostic

---
+++ src/Diagnostic.hs
@@ -0,0 +1,21 @@
+--
+-- EPITECH PROJECT, 2023
+-- B-FUN-500-PAR-5-2-glados-jeremy.elalouf
+-- File description:
+-- Diagnostic
+--
+
+module Diagnostic where
+
+import Token
+
+data Severity = Info
+              | Warning
+              | Error
+              deriving (Show, Eq)
+
+data Diagnostic = Diagnostic {
+    severity :: Severity,
+    message  :: String,
+    token    :: Maybe Token
+} deriving (Show)
