14f87036848cdf3e054e7e12b45869b2c4091a2f
Emil Pedersen <emil.pedersen@epitech.eu>
2023-12-24 01:45:10 +0100

    test: use new type creation functions in tests

--- test/EKTyping.hs
+++ test/EKTyping.hs
@@ -15,36 +15,41 @@ import Data.Semigroup (Semigroup(stimes))
 tests :: Test
 tests = test
   [ "bool" ~: do
-      let false = AtomTy "false"
-      let true = AtomTy "true"
-      let bool = concrete false <> concrete true
+      let false = atomTy "false"
+      let true = atomTy "true"
+      let bool = false <> true
       show bool @?= "false | true"
-      show (concrete false) @?= "false"
-      show (concrete true) @?= "true"
+      show false @?= "false"
+      show true @?= "true"
   , "any" ~: do
       show AnyTy @?= "Any"
-      show (AnyTy <> concrete (AtomTy "false")) @?= "Any"
-      show (concrete (AtomTy "false") <> AnyTy) @?= "Any"
+      show (AnyTy <> atomTy "false") @?= "Any"
+      show (atomTy "false" <> AnyTy) @?= "Any"
   , "function" ~: do
-      let f = FunctionTy (concrete (AtomTy "false")) (concrete (AtomTy "true"))
+      let f = functionTy (atomTy "false") (atomTy "true")
       show f @?= "(false -> true)"
   , "struct" ~: do
-      let s = StructTy "foo" [Field "bar" (concrete (AtomTy "false"))]
+      let s = structTy "foo" [Field "bar" (atomTy "false")]
       show s @?= "foo"
   , "merge" ~: do
-      let bool = concrete (AtomTy "false") <> concrete (AtomTy "true")
-      let tribool = bool <> concrete (AtomTy "undefined")
+      let bool = atomTy "false" <> atomTy "true"
+      let tribool = bool <> atomTy "undefined"
       show tribool @?= "false | true | undefined"
       let tribool2 = bool <> tribool
       tribool2 @?= tribool
-      let complex = mconcat [bool, tribool, concrete (AtomTy "nil"), bool]
+      let complex = mconcat [bool, tribool, atomTy "nil", bool]
       show complex @?= "false | nil | true | undefined"
       stimes (3 :: Integer) bool @?= bool
       stimes (0 :: Integer) tribool @?= mempty
   , "ints" ~: do
-      let c = concrete . IntTy
+      let c = intTy
       show (c 0 <> c 2) @?= "0 | 2"
       show (c 0 <> c 1) @?= "[0..1]"
       show (c 0 <> c 1 <> c 2 <> c (-1)) @?= "[-1..2]"
       show (c 0 <> c 1 <> c 2 <> c 27) @?= "[0..2] | 27"
+  , "int ranges" ~: do
+      show (intRangeTy 0 2) @?= "[0..2]"
+      show (intRangeFromTy 0) @?= "[0..]"
+      show (intRangeUpToTy 2) @?= "[..2]"
+      show intRangeInfTy @?= "[..]"
   ]
