c890f9c0db388a8602a959609843653fb4486200
Emil Pedersen <emil.pedersen@epitech.eu>
2023-12-26 16:39:52 +0100

    feat: add custom show implementations to ast

--- src/EK/Ast.hs
+++ src/EK/Ast.hs
@@ -8,40 +8,90 @@
 module EK.Ast
   ( Expr(..)
   , Stmt(..)
+  , Symbol(..)
+  , FunctionName(..)
   ) where
+import Data.List (intercalate)
+
+data Symbol
+  = Symbol String
+  | Placeholder
+
+newtype FunctionName = FunctionName [Symbol]
 
 data Expr
-  = IntLit Integer
+  = IntegerLit Integer
   | StringLit String
-  | Call String [Expr]
-  | ParenExpr Expr
-  | Placeholder
-  deriving (Show)
+  | Call FunctionName [CallItem]
+
+data CallItem
+  = ExprCall Expr
+  | PlaceholderCall
 
 data Stmt
   = AtomDef String
-  | Define String Expr
-  | EKExpr Expr
   | TypeDef String Type
   | StructDef String [StructElem]
   | FuncDef FuncPattern Expr
-  | ExternFunc FuncPattern
-  deriving (Show)
+  | ExternDef FuncPattern
 
 data StructElem = StructElem String Type
-  deriving (Show)
 
 data Type
   = TypeName String
   | IntRange (Maybe Integer) (Maybe Integer)
   | UnionType Type Type
-  deriving (Show)
 
 data FuncPattern = FuncPattern [FuncPatternItem] (Maybe Type)
-  deriving (Show)
 
 data FuncPatternItem
   = ArgPattern String (Maybe Type)
-  | IdentifierPattern String
+  | SymbolPattern String
   | PlaceholderPattern
-  deriving (Show)
+
+instance Show FunctionName where
+  show (FunctionName symbols) = unwords (show <$> symbols)
+
+instance Show Symbol where
+  show (Symbol s) = s
+  show Placeholder = "_"
+
+instance Show Expr where
+  show (IntegerLit i) = show i
+  show (StringLit s) = show s
+  show (Call (FunctionName name) items) = showCall name items
+
+showCall :: [Symbol] -> [CallItem] -> String
+showCall ((Symbol s):xs) i = s ++ " " ++ showCall xs i
+showCall (Placeholder:xs) ((ExprCall e):is) = "(" ++ show e ++ ") " ++ showCall xs is
+showCall (Placeholder:xs) (PlaceholderCall:is) = "_ " ++ showCall xs is
+showCall [] _ = ""
+showCall _ [] = "#error#"
+
+instance Show Stmt where
+  show (AtomDef s) = "atom " ++ s
+  show (TypeDef s t) = "type " ++ s ++ " = " ++ show t
+  show (StructDef s elems) = "struct " ++ s ++ " { " ++ intercalate ", " (show <$> elems) ++ " }"
+  show (FuncDef pattern expr) = "fn " ++ show pattern ++ " = " ++ show expr
+  show (ExternDef pattern) = "extern fn " ++ show pattern
+  
+instance Show StructElem where
+  show (StructElem s t) = s ++ " : " ++ show t
+
+instance Show Type where
+  show (TypeName s) = s
+  show (IntRange (Just a) (Just b)) = "[" ++ show a ++ ".." ++ show b ++ "]"
+  show (IntRange (Just a) Nothing) = "[" ++ show a ++ "..]"
+  show (IntRange Nothing (Just b)) = "[.." ++ show b ++ "]"
+  show (IntRange Nothing Nothing) = "[..]"
+  show (UnionType a b) = show a ++ " | " ++ show b
+
+instance Show FuncPattern where
+  show (FuncPattern items (Just t)) = unwords (show <$> items) ++ " : " ++ show t
+  show (FuncPattern items Nothing) = unwords (show <$> items)
+  
+instance Show FuncPatternItem where
+  show (ArgPattern s (Just t)) = "(" ++ s ++ " : " ++ show t ++ ")"
+  show (ArgPattern s Nothing) = "(" ++ s ++ ")"
+  show (SymbolPattern s) = s
+  show PlaceholderPattern = "_"
