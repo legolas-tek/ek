2f571520f441f28babbaae8f12090a17f1d17ad9
LindonAliu <lindon.aliu@epitech.eu>
2023-12-28 00:32:26 +0100

    feat: add the compiler for FuncDefs

--- src/EK/Compiler.hs
+++ src/EK/Compiler.hs
@@ -12,40 +12,36 @@ module EK.Compiler
 import VirtualMachine
 import EK.Ast
 
-import Data.Map (Map, lookup, empty)
+import Data.Map (empty)
 
-compileToVM :: [Stmt] -> [Expr] -> Either String Insts
-compileToVM stmts exprs = do
-  stmtInsts <- compileStmts stmts empty
-  exprInsts <- compileExprs exprs empty
-  return $ stmtInsts ++ exprInsts
+compileToVM :: [Stmt] -> Either String Insts
+compileToVM stmts = compileFuncDefs stmts empty
 
-compileExprs :: [Expr] -> Env -> Either String Insts
-compileExprs [] env = Right []
-compileExprs (expr:exprs) env =
-  case compileExpr expr env of
+compileFuncDefs :: [Stmt] -> Env -> Either String Insts
+compileFuncDefs [] _ = Right []
+compileFuncDefs (stmt:rest) env =
+  case compileFuncDef stmt env of
     Left err -> Left err
     Right insts -> do
-      restInsts <- compileExprs exprs env
+      restInsts <- compileFuncDefs rest env
       return (insts ++ restInsts)
+
+compileFuncDef :: Stmt -> Env -> Either String Insts
+compileFuncDef (FuncDef pattern expr) env = do
+  exprInsts <- compileExpr expr env
+  let patternInsts = compilePattern pattern env
+  return (patternInsts ++ exprInsts ++ [Ret])
+compileFuncDef _ _ = Right []
+
+compilePattern :: FuncPattern -> Env -> Insts
+compilePattern (FuncPattern items _) _ =
+  concatMap compilePatternItem items
+  where
+    compilePatternItem (ArgPattern s _) = [PushEnv s]
+    compilePatternItem (SymbolPattern s) = [PushEnv s]
+    compilePatternItem PlaceholderPattern = []
 
 compileExpr :: Expr -> Env -> Either String Insts
 compileExpr (IntegerLit i) _ = Right [Push (IntegerValue i)]
 compileExpr (StringLit s) _ = Right [Push (StringValue s)]
-compileExpr (EK.Ast.Call name callItems) env = Right []
-
-compileStmts :: [Stmt] -> Env -> Either String Insts
-compileStmts [] env = Right []
-compileStmts (stmt:rest) env =
-  case compileStmt stmt env of
-    Left err -> Left err
-    Right insts -> do
-      restInsts <- compileStmts rest env
-      return (insts ++ restInsts)
-
-compileStmt :: Stmt -> Env -> Either String Insts
-compileStmt (AtomDef _) env = Right []
-compileStmt (TypeDef _ _) env = Right []
-compileStmt (StructDef _ _) env = Right []
-compileStmt (FuncDef pattern expr) env = Right []
-compileStmt (ExternDef pattern) env = Right []
+compileExpr (EK.Ast.Call _ _) _ = Right []
