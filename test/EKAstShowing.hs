fe427eca7fbbc1dc93a946c66380937af3afc0e9
Yunse LEE <yunselee@gmail.com>
2024-01-02 18:05:23 +0100

    fix: build err for test

--- test/EKAstShowing.hs
+++ test/EKAstShowing.hs
@@ -40,11 +40,11 @@ tests = test
       show' (FuncDef (FuncPattern [SymbolPattern "foo"] Nothing Nothing) (IntegerLit 42)) @?= "fn foo = 42"
       show' (FuncDef (FuncPattern [SymbolPattern "foo"] (Just (TypeName "bar")) Nothing) (IntegerLit 42)) @?= "fn foo : bar = 42"
       show' (FuncDef (FuncPattern [SymbolPattern "foo", SymbolPattern "bar"] Nothing Nothing) (IntegerLit 42)) @?= "fn foo bar = 42"
-      show' (FuncDef (FuncPattern [ArgPattern "a" Nothing, SymbolPattern "+", ArgPattern "b" Nothing] Nothing Nothing) (IntegerLit 42)) @?= "fn (a) + (b) = 42"
-      show' (FuncDef (FuncPattern [ArgPattern "a" Nothing, SymbolPattern "+", ArgPattern "b" Nothing] Nothing (Just 6)) (IntegerLit 42)) @?= "fn (a) + (b) precedence 6 = 42"
-      show' (FuncDef (FuncPattern [ArgPattern "a" Nothing, SymbolPattern "+", ArgPattern "b" Nothing] (Just $ TypeName "int") (Just 6)) (IntegerLit 42)) @?= "fn (a) + (b) : int precedence 6 = 42"
+      show' (FuncDef (FuncPattern [ArgPattern False "a" Nothing, SymbolPattern "+", ArgPattern False "b" Nothing] Nothing Nothing) (IntegerLit 42)) @?= "fn (a) + (b) = 42"
+      show' (FuncDef (FuncPattern [ArgPattern False "a" Nothing, SymbolPattern "+", ArgPattern False "b" Nothing] Nothing (Just 6)) (IntegerLit 42)) @?= "fn (a) + (b) precedence 6 = 42"
+      show' (FuncDef (FuncPattern [ArgPattern False "a" Nothing, SymbolPattern "+", ArgPattern False "b" Nothing] (Just $ TypeName "int") (Just 6)) (IntegerLit 42)) @?= "fn (a) + (b) : int precedence 6 = 42"
   , "extern def" ~: do
-      show' (ExternDef (FuncPattern [SymbolPattern "exit", ArgPattern "code" (Just $ TypeName "int")] (Just $ TypeName "never") Nothing)) @?= "extern fn exit (code : int) : never"
+      show' (ExternDef (FuncPattern [SymbolPattern "exit", ArgPattern False "code" (Just $ TypeName "int")] (Just $ TypeName "never") Nothing)) @?= "extern fn exit (code : int) : never"
   , "function names" ~: do
       show (FunctionName [Symbol "foo"] defaultPrec) @?= "foo"
       show (FunctionName [Symbol "foo", Placeholder] defaultPrec) @?= "foo _"
