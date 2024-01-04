949bcee7bc30c05bd9a1000fab6df9cc318e9458
BADER Marouan <marouan.bader@epitech.eu>
2024-01-04 15:40:35 +0100

    feat: add tests for serializing

--- test/Serializing.hs
+++ test/Serializing.hs
@@ -15,11 +15,25 @@ import VirtualMachine
 
 import Serialize
 
-import qualified Data.ByteString as B
+-- import qualified Data.ByteString()
 
 tests :: Test
 tests = test
   [ "serialize" ~: do
-    --   serialize Push (IntegerValue 3) @?= []
+      serialize (Push (IntegerValue 49)) @?= "\SOH\SOH49\0"
+      serialize (Push (AtomValue "test")) @?= "\SOH\STXtest\0"
+      serialize (Push (StringValue "test str")) @?= "\SOH\ETXtest str\0"
+      serialize (Push (FunctionValue [Ret])) @?= "\SOH\0"
+      serialize Call @?= "\STX"
+      serialize (CallOp Add)  @?= "\ETX"
+      serialize (CallOp Sub)  @?= "\EOT"
+      serialize (CallOp Mul)  @?= "\ENQ"
+      serialize (CallOp Div)  @?= "\ACK"
+      serialize (CallOp Eq)  @?= "\BEL"
+      serialize (CallOp Less)  @?= "\BS"
+      serialize (JmpFalse 49)  @?= "\t1"
+      serialize Dup @?= "\n"
       serialize Ret @?= "\v"
+      serialize (LoadArg 49) @?= "\f1"
+      serialize (PopEnv "test") @?= "\rtest\0"
   ]
