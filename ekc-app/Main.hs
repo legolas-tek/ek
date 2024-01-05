6a58bfd25f739059eb134bd7f820e2701ba33748
Emil Pedersen <emil.pedersen@epitech.eu>
2024-01-05 17:34:18 +0100

    feat: add argument parsing

--- ekc-app/Main.hs
+++ ekc-app/Main.hs
@@ -5,24 +5,35 @@
 -- Main for ekc
 --}
 
-{-# LANGUAGE TupleSections #-}
-
 module Main (main) where
 
-import System.IO
-import System.Environment
-
+import ArgParser
 import Tokenizer
 import EK.Parser
 import EK.Compiler
 
+import Data.Maybe (fromMaybe)
+
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+import Control.Monad (when)
+
+readFileOrStdIn :: Maybe String -> IO String
+readFileOrStdIn Nothing = getContents
+readFileOrStdIn (Just file) = readFile file
+
+output :: String -> IO ()
+output o = putStrLn o >> exitSuccess
+
 main :: IO ()
 main = do
-  args <- getArgs
-  let file = head args
-  content <- readFile file
-  tokens <- either (fail . show) return $ tokenizer file content
+  args' <- getArgs
+  arg <- either (fail . show) return $ parseArguments args'
+  content <- readFileOrStdIn $ argInput arg
+  tokens <- either (fail . show) return $ tokenizer ("stdin" `fromMaybe` argInput arg) content
+  when (argOutputType arg == Just OutputTokens) $ output $ show tokens
   ast <- either (fail . show) return $ parseDocument tokens
+  when (argOutputType arg == Just OutputAst) $ output $ show ast
   insts <- either fail return $ compileToVM ast
   putStrLn $ showBytecode insts
   return ()
