aa75f1877348fc4fe63f3b81396302d1d7544d25
BADER Marouan <marouan.bader@epitech.eu>
2024-01-06 19:25:58 +0100

    refactor: add a type class serializable

--- src/Serialize.hs
+++ src/Serialize.hs
@@ -13,6 +13,9 @@ import VirtualMachine
 import Data.String(IsString(..))
 import EK.Compiler(Result)
 
+class Serializable a where
+  serialize :: a -> B.ByteString
+
 writeFunc :: (String, Insts) -> String -> IO ()
 writeFunc (key, insts) path =
     B.writeFile (path ++ ".eko") (fromString key <> B.singleton 0 <>
@@ -23,25 +26,24 @@ saveResult result path =
     B.writeFile (path ++ ".eko") (B.singleton 42) >>
     mapM_ (\pair -> writeFunc pair path) (Map.toList result)
 
+instance Serializable Instruction where
+  serialize (Push value) = B.singleton 1 <> serialize value
+  serialize Call = B.singleton 2
+  serialize (CallOp Add) = B.singleton 3
+  serialize (CallOp Sub) = B.singleton 4
+  serialize (CallOp Mul) = B.singleton 5
+  serialize (CallOp Div) = B.singleton 6
+  serialize (CallOp Eq) = B.singleton 7
+  serialize (CallOp Less) = B.singleton 8
+  serialize (JmpFalse jmpValue) = B.singleton 9 <> B.singleton (fromIntegral jmpValue)
+  serialize Dup = B.singleton 10
+  serialize Ret = B.singleton 11
+  serialize (LoadArg value) = B.singleton 12 <> B.singleton (fromIntegral value)
+  serialize (GetEnv value) = B.singleton 13 <> fromString value <> B.singleton 0
+  serialize (CallOp Print) = B.singleton 14
 
-serialize :: Instruction -> B.ByteString
-serialize (Push value) = B.singleton 1 <> serializeVMValue value
-serialize Call = B.singleton 2
-serialize (CallOp Add) = B.singleton 3
-serialize (CallOp Sub) = B.singleton 4
-serialize (CallOp Mul) = B.singleton 5
-serialize (CallOp Div) = B.singleton 6
-serialize (CallOp Eq) = B.singleton 7
-serialize (CallOp Less) = B.singleton 8
-serialize (JmpFalse jmpValue) = B.singleton 9 <> B.singleton (fromIntegral jmpValue)
-serialize Dup = B.singleton 10
-serialize Ret = B.singleton 11
-serialize (LoadArg value) = B.singleton 12 <> B.singleton (fromIntegral value)
-serialize (GetEnv value) = B.singleton 13 <> fromString value <> B.singleton 0
-serialize (CallOp Print) = B.singleton 14
-
-serializeVMValue :: VMValue -> B.ByteString
-serializeVMValue (IntegerValue value) = B.singleton 1 <> fromString (show value) <> B.singleton 0
-serializeVMValue (AtomValue value) = B.singleton 2 <> fromString value <> B.singleton 0
-serializeVMValue (StringValue value) = B.singleton 3 <> fromString value <> B.singleton 0
-serializeVMValue (FunctionValue _) = B.singleton 0
+instance Serializable VMValue where
+  serialize (IntegerValue value) = B.singleton 1 <> fromString (show value) <> B.singleton 0
+  serialize (AtomValue value) = B.singleton 2 <> fromString value <> B.singleton 0
+  serialize (StringValue value) = B.singleton 3 <> fromString value <> B.singleton 0
+  serialize (FunctionValue _) = B.singleton 0
