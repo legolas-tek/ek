f817b7fdeea4127b1c8d51203f8dee7bd0aff29f
Emil Pedersen <emil.pedersen@epitech.eu>
2024-01-07 21:07:17 +0100

    test: replace Lisp with EK in functional tests

--- src/EK/Builtins.hs
+++ src/EK/Builtins.hs
@@ -19,9 +19,11 @@ void = Push $ AtomValue "void"
 
 builtins :: Result
 builtins = Map.fromList
-  [
-    ("builtin print", [GetEnv "builtin print\\impl", Ret]),
-    ("builtin print\\impl", [LoadArg 0, CallOp Print, void, Ret]),
-    ("builtin exit", [GetEnv "builtin exit\\impl", Ret]),
-    ("builtin exit\\impl", [LoadArg 0, CallOp Exit, void, Ret])
+  [ ("builtin print", [GetEnv "builtin print\\impl", Ret])
+  , ("builtin print\\impl", [LoadArg 0, CallOp Print, void, Ret])
+  , ("builtin exit", [GetEnv "builtin exit\\impl", Ret])
+  , ("builtin exit\\impl", [LoadArg 0, CallOp Exit, void, Ret])
+  , ("builtin add", [GetEnv "builtin add\\a", Ret])
+  , ("builtin add\\a", [LoadArg 0, GetEnv "builtin add\\b", Closure 1, Ret])
+  , ("builtin add\\b", [LoadArg 0, LoadArg 1, CallOp Add, Ret])
   ]
