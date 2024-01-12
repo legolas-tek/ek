9462dabfe0047b31916d366fa1ec51ab8213b48e
LindonAliu <lindon.aliu@epitech.eu>
2024-01-12 23:45:41 +0100

    feat(optimization): merge functions with the same content

--- src/EK/Optimizer.hs
+++ src/EK/Optimizer.hs
@@ -13,17 +13,32 @@ module EK.Optimizer
 import VirtualMachine
 import EK.Compiler
 
-import qualified Data.Map as Map (lookup, filterWithKey, foldrWithKey, keys, filter)
-import qualified Data.Maybe as Maybe (fromMaybe)
+import qualified Data.Map as Map (lookup, filterWithKey, foldrWithKey, mapWithKey, map, keys)
 import qualified Data.Set as Set
 import Data.List (nub)
-import Debug.Trace
-import EK.Compiler (Result)
 
-getUsedFunctions :: Insts -> [String]
-getUsedFunctions [] = []
-getUsedFunctions (GetEnv x : xs) = x : getUsedFunctions xs
-getUsedFunctions (_ : xs) = getUsedFunctions xs
+optimizeBytecode :: Result -> Result
+optimizeBytecode = fmap optimizeInsts . deleteNotUsedFunc .  deleteSameInstsOfFunc . deleteNotUsedFunc
+
+optimizeInsts :: Insts -> Insts
+optimizeInsts [] = []
+optimizeInsts (Closure 0: rest) = optimizeInsts rest
+optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Add : rest) = Push (IntegerValue (x + y)) : optimizeInsts rest
+optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Sub : rest) = Push (IntegerValue (x - y)) : optimizeInsts rest
+optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Mul : rest) = Push (IntegerValue (x * y)) : optimizeInsts rest
+optimizeInsts (inst : rest) = inst : optimizeInsts rest
+
+deleteNotUsedFunc :: Result -> Result
+deleteNotUsedFunc insts = deleteNotUsedFunc' insts (getCalledFunctionsFromMain insts)
+
+deleteNotUsedFunc' :: Result -> [String] -> Result
+deleteNotUsedFunc' insts functionsUsed = Map.filterWithKey (\k _ -> k `elem` functionsUsed) insts
+
+getCalledFunctionsFromMain :: Result -> [String]
+getCalledFunctionsFromMain insts = "main" : getCalledFunctions insts "main"
+
+getCalledFunctions :: Result -> String -> [String]
+getCalledFunctions insts func = visitCalledFunctions insts Set.empty [func]
 
 visitCalledFunctions :: Result -> Set.Set String -> [String] -> [String]
 visitCalledFunctions _ _ [] = []
@@ -35,39 +50,32 @@ visitCalledFunctions insts visited (x : xs)
         Just insts' -> let calledFuncs = getUsedFunctions insts'
                        in nub $ calledFuncs ++ visitCalledFunctions insts (Set.insert x visited) (xs ++ calledFuncs)
 
-getCalledFunctions :: Result -> String -> [String]
-getCalledFunctions insts func = visitCalledFunctions insts Set.empty [func]
+getUsedFunctions :: Insts -> [String]
+getUsedFunctions [] = []
+getUsedFunctions (GetEnv x : xs) = x : getUsedFunctions xs
+getUsedFunctions (_ : xs) = getUsedFunctions xs
 
-getCalledFunctionsFromMain :: Result -> [String]
-getCalledFunctionsFromMain insts = "main" : getCalledFunctions insts "main"
+deleteSameInstsOfFunc :: Result -> Result
+deleteSameInstsOfFunc insts = deleteSameInstsOfFunc' insts (Map.keys insts)
 
-deleteNotUsedFunc :: Result -> Result
-deleteNotUsedFunc insts = deleteNotUsedFunc' insts (getCalledFunctionsFromMain insts)
+deleteSameInstsOfFunc' :: Result -> [String] -> Result
+deleteSameInstsOfFunc' insts [] = insts
+deleteSameInstsOfFunc' insts (x : xs) =
+  case Map.lookup x insts of
+    Nothing -> deleteSameInstsOfFunc' insts xs
+    Just insts' ->
+      let sameInsts = detectSameInsts x insts' insts
+      in if null sameInsts
+         then deleteSameInstsOfFunc' insts xs
+         else deleteSameInstsOfFunc' (changeFuncNameInInsts x sameInsts insts) xs
 
 detectSameInsts :: String -> Insts -> Result -> [String]
 detectSameInsts fname insts = Map.foldrWithKey (\k v acc -> if k /= fname && v == insts then k : acc else acc) []
 
-changeFuncNameInInsts :: String -> String -> Insts -> Insts
-changeFuncNameInInsts _ _ [] = []
-changeFuncNameInInsts oldName newName (GetEnv x : xs) = GetEnv (if x == oldName then newName else x) : changeFuncNameInInsts oldName newName xs
-changeFuncNameInInsts oldName newName (x :xs) = x : changeFuncNameInInsts oldName newName xs
+updateFuncName :: String -> [String] -> Insts -> Insts
+updateFuncName _ _ [] = []
+updateFuncName name namesToChange (GetEnv x : xs) = GetEnv (if x `elem` namesToChange then name else x) : updateFuncName name namesToChange xs
+updateFuncName name namesToChange (x : xs) = x : updateFuncName name namesToChange xs
 
--- replaceFuncName :: String -> [String] -> Result -> Result
--- replaceFuncName _ [] insts = insts
--- replaceFuncName newName (oldName : xs) insts =
-
-deleteNotUsedFunc' :: Result -> [String] -> Result
-deleteNotUsedFunc' insts functionUsed = Map.filterWithKey (\k _ -> k `elem` functionUsed) insts
-
-optimizeBytecode :: Result -> Result
-optimizeBytecode res =
-  let printInsts = Maybe.fromMaybe [] (Map.lookup "print _" res)
-  in trace (show $ detectSameInsts "print _" printInsts res) (fmap optimizeInsts (deleteNotUsedFunc res))
-
-optimizeInsts :: Insts -> Insts
-optimizeInsts [] = []
-optimizeInsts (Closure 0: rest) = optimizeInsts rest
-optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Add : rest) = Push (IntegerValue (x + y)) : optimizeInsts rest
-optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Sub : rest) = Push (IntegerValue (x - y)) : optimizeInsts rest
-optimizeInsts (Push (IntegerValue x) : Push (IntegerValue y) : CallOp Mul : rest) = Push (IntegerValue (x * y)) : optimizeInsts rest
-optimizeInsts (inst : rest) = inst : optimizeInsts rest
+changeFuncNameInInsts :: String -> [String] -> Result -> Result
+changeFuncNameInInsts name namesToChange = Map.mapWithKey (\_ v -> updateFuncName name namesToChange v)
