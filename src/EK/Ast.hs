01850085837d52af4d5da5400d51f902bc63fd03
Emil Pedersen <emil.pedersen@epitech.eu>
2024-01-13 23:55:31 +0100

    feat: add typechecks to ast

--- src/EK/Ast.hs
+++ src/EK/Ast.hs
@@ -53,6 +53,7 @@ data Expr' typeval
   | Call FunctionName [Expr' typeval]
   | Lambda String (Expr' typeval)
   | StructLit typeval [Expr' typeval]
+  | TypeCheck (Expr' typeval) typeval
   deriving (Eq)
 
 type Expr = Expr' Type
@@ -147,6 +148,7 @@ instance Show typeval => Show (Expr' typeval) where
   show (Call (FunctionName name _) items) = "(" ++ unwords (showCall name items) ++ ")"
   show (Lambda arg expr) = "(\\" ++ arg ++ " = " ++ show expr ++ ")"
   show (StructLit s elems) = show s ++ " { " ++ intercalate ", " (show <$> elems) ++ " }"
+  show (TypeCheck expr t) = "(" ++ show expr ++ " is " ++ show t ++ ")"
 
 showCall :: Show typeval => [Symbol] -> [Expr' typeval] -> [String]
 showCall ((Symbol s):xs) i = s : showCall xs i
